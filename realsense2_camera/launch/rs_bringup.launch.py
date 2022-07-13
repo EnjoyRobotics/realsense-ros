57458668ab1a4b02ec0118486f73574ae8ae2856
redvinaa <redvinaa@gmail.com>
2022-07-13 14:41:29 +0200

    Fix use_sim_time

--- realsense2_camera/launch/rs_bringup.launch.py
+++ realsense2_camera/launch/rs_bringup.launch.py
@@ -1,85 +1,69 @@
 import os
+from distutils.util import strtobool
 
 from ament_index_python.packages import get_package_share_directory
 
-from launch import LaunchDescription, Substitution, LaunchContext
-from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler, DeclareLaunchArgument, SetEnvironmentVariable, OpaqueFunction
+from launch import LaunchDescription
+from launch.actions import RegisterEventHandler
+from launch.conditions import UnlessCondition
 from launch.event_handlers import OnProcessIO
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-from launch.conditions import IfCondition, UnlessCondition
-from launch_ros.actions import Node
-from launch.substitutions import EnvironmentVariable, PythonExpression
 
-from typing import Text
-import xacro
+from launch_ros.actions import Node
 
 counter = 1
 
-def generate_launch_description():
 
-	# args
-	arg_use_sim_time = LaunchConfiguration('use_sim_time')
-	arg_simulation   = LaunchConfiguration('simulation')
+def generate_launch_description() -> LaunchDescription:
+    simulation = bool(strtobool(os.environ.get('SIMULATION')))
 
-	simulation = os.environ.get('SIMULATION')
-	if simulation in [None, '']:
-		simulation = 'False'
+    realsense_cam_parameters = os.path.join(get_package_share_directory('realsense2_camera'), 'config', 'realsense.yaml')
 
-	output = 'screen'
+    preset_file = os.path.join(get_package_share_directory('realsense2_camera'), 'config', 'high_accuracy_preset.json')
 
-	realsense_cam_parameters = os.path.join(get_package_share_directory('realsense2_camera'), 'config', 'realsense.yaml')
+    t265_camera = Node(
+        package='realsense2_camera',
+        executable='realsense2_camera_node',
+        name='t265_camera',
+        namespace='t265_camera',
+        emulate_tty=True,
+        prefix=['stdbuf -o L'],
+        parameters=[realsense_cam_parameters, {'use_sim_time': simulation}],
+        remappings=[('odom/sample', 'odom')],
+        condition=UnlessCondition(str(simulation)),
+    )
 
-	preset_file = os.path.join(get_package_share_directory('realsense2_camera'), 'config', 'high_accuracy_preset.json')
+    d435_camera = Node(
+        package='realsense2_camera',
+        executable='realsense2_camera_node',
+        name='d435_camera',
+        namespace='d435_camera',
+        emulate_tty=True,
+        prefix=['stdbuf -o L'],
+        parameters=[realsense_cam_parameters, {'json_file_path': preset_file, 'use_sim_time': simulation}],
+        remappings=[('/d435_camera/depth/color/points', '/cloud_in')],
+        condition=UnlessCondition(str(simulation)),
+    )
 
-	t265_camera = Node(
-		package='realsense2_camera',
-		executable='realsense2_camera_node',
-		name='t265_camera',
-		namespace='t265_camera',
-		emulate_tty=True,
-		prefix=['stdbuf -o L'],
-		parameters=[realsense_cam_parameters, {'use_sim_time': arg_use_sim_time}],
-		remappings=[('odom/sample', 'odom')],
-		condition=UnlessCondition(arg_simulation),
-	)
+    def start_t265_camera(text: str) -> type(None):
+        global counter
+        if 'RealSense Node Is Up' in str(text):
+            counter += 1
+        if counter == 2:
+            counter = 3  # launch only once
+            ld = LaunchDescription([t265_camera])
+            return ld
+        else:
+            return
 
-	d435_camera = Node(
-		package='realsense2_camera',
-		executable='realsense2_camera_node',
-		name='d435_camera',
-		namespace='d435_camera',
-		emulate_tty=True,
-		prefix=['stdbuf -o L'],
-		parameters=[realsense_cam_parameters, {'json_file_path': preset_file, 'use_sim_time': arg_use_sim_time}],
-		remappings=[('/d435_camera/depth/color/points', '/cloud_in')],
-		condition=UnlessCondition(arg_simulation),
-	)
+    ld = LaunchDescription(
+        # args
+        [
+            d435_camera,
 
+            RegisterEventHandler(event_handler=OnProcessIO(
+                on_stderr=lambda info: start_t265_camera(info.text)
+            )),
+        ]
+    )
 
-	def start_t265_camera(text):
-		global counter
-		if 'RealSense Node Is Up' in str(text):
-			counter += 1
-		if counter == 2:
-			counter = 3 # launch only once
-			ld = LaunchDescription([t265_camera])
-			return ld
-		else:
-			return
-
-	ld = LaunchDescription(
-		# args
-		[
-			DeclareLaunchArgument('simulation',       default_value=simulation),
-			DeclareLaunchArgument('use_sim_time',     default_value='false'),
-			d435_camera,
-
-			RegisterEventHandler(
-			event_handler=OnProcessIO(
-				on_stderr=lambda info: start_t265_camera(info.text)
-			)),
-		]
-	)
-
-	return ld
+    return ld
